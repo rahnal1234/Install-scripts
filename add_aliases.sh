6dcb8da7c9842fbe3d1bb9224ca24e01570ed65b
rahnal <rahn-alexander@gmx.net>
2026-01-26 15:31:08 +0000

    Update add_aliases.sh
    Tippfehler behoben

--- add_aliases.sh
+++ add_aliases.sh
@@ -1,4 +1,4 @@
 echo "alias ..='cd ..'
 alias ll='ls -l'
-alias update='sudo dnf -y update && sudo dnf -y ugrade'
+alias update='sudo dnf -y update && sudo dnf -y upgrade'
 alias la='ls -la'" >> ~/.bash_aliases
